5890c4370158aebe94e1342b0f58740f4b99a9e9
marcosfelipp <marcosabreu@inf.ufg.br>
2021-10-21 14:07:06 -0300

    Update tags css

--- imoveis_crawling/imoveis_crawling/spiders/ImoveisCrawling.py
+++ imoveis_crawling/imoveis_crawling/spiders/ImoveisCrawling.py
@@ -8,7 +8,6 @@ import random
 class ImoveisCrawlingSpider(scrapy.Spider):
     name = 'IMOVEIS'
     allowed_domains = ['olx.com.br']
-    page_number = 1
 
     def __init__(self, category="aluguel", region=None, state=None, *args, **kwargs):
         super(ImoveisCrawlingSpider, self).__init__(*args, **kwargs)
@@ -16,24 +15,25 @@ class ImoveisCrawlingSpider(scrapy.Spider):
         self.region = region
         self.state = state
         self.start_urls = ['https://%s.olx.com.br/%s/imoveis/%s?sf=1' % (self.state, self.region, self.category)]
+        self.page_number = 1
 
     def parse(self, response):
 
-        for item in response.css("li.sc-1fcmfeb-2.ggOGTJ"):
-            id = item.css("a ::attr(data-lurker_list_id)").extract_first()
-            titulo = item.css("div.fnmrjs-8.kRlFBv ::text").extract_first()
-            regiao = item.css("p.fnmrjs-13.hdwqVC ::text").extract_first()
-            detalhes = item.css("p.jm5s8b-0.jDoirm ::text").extract_first()
-            preco = item.css("p.fnmrjs-16.jqSHIm ::text").extract_first()
-            data_de_publicacao = item.css("p.fnmrjs-19.eJIIxH ::text").extract_first()
+        for item in response.css("a.fnmrjs-0.fyjObc"):
+            id = item.css("::attr(data-lurker_list_id)").extract_first()
+            titulo = item.css("h2.sc-1mbetcw-0.fKteoJ.sc-ifAKCX.jyXVpA ::text").extract_first()
+            regiao = item.css("span.sc-7l84qu-1.ciykCV.sc-ifAKCX.dpURtf ::text").extract_first()
+            detalhes = item.css("span.sc-1j5op1p-0.lnqdIU.sc-ifAKCX.eLPYJb ::text").extract_first()
+            preco = item.css(" span.sc-ifAKCX.eoKYee ::text").extract_first()
+            data_de_publicacao = item.css("span.wlwg1t-1.fsgKJO.sc-ifAKCX.eLPYJb ::text").extract_first()
 
             imovel = ImoveisCrawlingItem(id=id, titulo=titulo, regiao=regiao, detalhes=detalhes, preco=preco,
                                          data_de_publicacao=data_de_publicacao)
             yield imovel
 
-        time.sleep(random.randint(15, 20))
+        time.sleep(random.randint(5, 15))
         self.page_number += 1
 
-        if self.page_number < 70:
+        if self.page_number < 100:
             next_page = self.start_urls[0] + '&o=' + str(self.page_number)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield scrapy.Request(next_page, callback=self.parse, dont_filter=True)
